d9188fa5bf57f057e225b81f1a86bd8fa04b2696
Rakuichi4817 <m.naoki4817@gmail.com>
2020-01-17 16:11:09 +0900

    Add method

--- CaboCha.py
+++ CaboCha.py
@@ -11,12 +11,12 @@ class CaboCha:
         self.proc = subprocess.Popen([exe,'-f1'],shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         self.encoding = encoding
     
-    def tree2string(self, text):
+    def tree2string(self, sentence):
         """
         解析の実行結果を標準出力のまま受け取るメソッド
-        @ text: 係り受け解析の対象となる文字列
+        @ sentence: 係り受けしたい文
         """
-        self.proc.stdin.write(bytes(text + '\n', self.encoding))
+        self.proc.stdin.write(bytes(sentence + '\n', self.encoding))
         self.proc.stdin.flush()
         # 出力を取得
         cabocha_out = []
@@ -28,12 +28,12 @@ class CaboCha:
                 
         return "".join(cabocha_out)
     
-    def tree2list(self, text):
+    def tree2list(self, sentence):
         """
         解析の結果をリスト化して返す。
-        @ text: 係り受け解析の対象となる文字列
+        @ sentence: 係り受けしたい文
         """
-        cabocha_out = self.tree2string(text)
+        cabocha_out = self.tree2string(sentence)
         # 文節ごとにリストで持つ
         phrases = [line.rstrip("\r\n") for line in cabocha_out.rstrip("\r\nEOS").split("* ") if line != ""]
         result = [] # 結果
@@ -70,3 +70,63 @@ class CaboCha:
             }
             result.append(add_dict)
         return result
+    
+    def get_bf_morph_pairs(self, sentence):
+        '''
+        係り受け関係にある文節より基本形にした形態素ペアをリストで取得
+        @ sentence: 係り受けしたい文
+        '''
+        phrases = self.tree2list(sentence)
+        out_morph_pairs = list()
+        # 文節毎に処理
+        for pidx, phrase_data in enumerate(phrases):
+            ahead_pidx = phrase_data["ahead_pidx"]
+            main_midx = phrase_data["main_midx"]
+            morphs = phrase_data["morphs"]
+            # 係り先であれば何もしない
+            if ahead_pidx != -1:
+                dep_morphs = phrases[ahead_pidx]["morphs"]  # 係り先の形態素リスト
+                dep_main_midx = phrases[ahead_pidx]["main_midx"]  # 係り先の主辞
+
+                # 係り元の主辞が名詞であれば連続する名詞を原型にして取得
+                if morphs[main_midx]["pos"] == "名詞" and not morphs[main_midx]["pos1"].startswith("形容動詞"):
+                    morph_out = "".join([morph["surface"] for morph in morphs if morph["pos"] == "名詞"])
+                else:
+                    morph_out = morphs[main_midx]["base"]
+
+                # 係り先の主辞が名詞であれば連続する名詞を原型にして取得
+                if dep_morphs[dep_main_midx]["pos"] == "名詞" and not dep_morphs[dep_main_midx]["pos1"].startswith("形容動詞"):
+                    dep_morph_out = "".join([dep_morph["surface"] for dep_morph in dep_morphs if dep_morph["pos"] == "名詞"])
+                # 係り先が動詞の場合は前後を見る
+                elif dep_morphs[dep_main_midx]["pos"].startswith("動詞"):
+                    # 主辞の前を見ていく
+                    count = 1
+                    temp_morphs = [dep_morphs[dep_main_midx]["base"]]
+                    while True:
+                        check_idx = dep_main_midx - count
+                        if check_idx < 0:
+                            break
+                        elif dep_morphs[check_idx]["pos"].startswith("名詞") or dep_morphs[check_idx]["pos"].startswith("動詞"):
+                            temp_morphs.insert(0, dep_morphs[check_idx]["base"])
+                        else:
+                            break
+                        count += 1
+                    
+                    count = 1   
+                    while True:
+                        check_idx = dep_main_midx + count
+                        if check_idx == len(morphs):
+                            break
+                        elif dep_morphs[check_idx]["pos"].startswith("名詞") or dep_morphs[check_idx]["pos"].startswith("動詞"):
+                            temp_morphs.append(dep_morphs[check_idx]["base"])
+                        else:
+                            break
+                        count += 1
+                    dep_morph_out = "".join(temp_morphs)
+                else:
+                    dep_morph_out = dep_morphs[dep_main_midx]["base"]
+
+                one_pair = [morph_out, dep_morph_out]
+                out_morph_pairs.append(one_pair)
+
+        return out_morph_pairs
